e0b0d573baac9d75b6b6b60d461bbdca6dc38422
Jeremiasz Kubryn <jeremiasz.kubryn@partnerspol.pl>
2023-05-15 15:17:07 +0200

    Add project files.

---
+++ GUSRekrutacja/ThematicAreas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GUSRekrutacja
+{
+    public class ThematicAreas
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+        [JsonPropertyName("nazwa")]
+        public string Name { get; set; }
+        [JsonPropertyName("id-nadrzedny-element")]
+        public int IDSuperiorelement { get; set; }
+        [JsonPropertyName("id-poziom")]
+        public int IDLevel { get; set; }
+        [JsonPropertyName("nazwa-poziom")]
+        public string NameLevel { get; set; }
+        [JsonPropertyName("czy-zmienne")]
+        public int Variables { get; set; }
+    }
+}
